c92a9c9421274104c60c30fa23a1f30a7bf8baae
3maigenkou <kaipurio848@gmail.com>
2021-09-07 12:36:26 +0900

    テストです

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,13 +5,13 @@ require 'optparse'
 
 def main
   options = ARGV.getopts('l')
-  file_name = ARGV.empty? ? $stdin.readlines.map(&:chomp) : ARGV
-  options['l'] ? output_wc_l(file_name) : output_wc(file_name)
+  input_str = ARGV.empty? ? $stdin.readlines.map(&:chomp) : ARGV
+  options['l'] ? output_wc_l(input_str) : output_wc(input_str)
 end
 
-def output_wc(file_name)
-  if File.exist?(file_name[0])
-    file_name.each do |a|
+def output_wc(input_str)
+  if File.exist?(input_str[0])
+    input_str.each do |a|
       file = File.read(a)
       file_lines = file.count("\n").to_s.rjust(8)
       words = file.split(/\s+/).size.to_s.rjust(7)
@@ -19,52 +19,48 @@ def output_wc(file_name)
       name = File.basename(a)
       puts "#{file_lines} #{words} #{bytesize} #{name}"
     end
-    return unless file_name.size > 1
+    return unless input_str.size > 1
 
-    total_lines_point(file_name)
-    total_words_point(file_name)
-    total_bytesize_point(file_name)
+    total_lines_point(input_str)
+    total_words_point(input_str)
+    total_bytesize_point(input_str)
     print " total\n"
 
   else
-    total_lines_point_nofile(file_name)
-    total_words_point_nofile(file_name)
-    total_bytesize_point_nofile(file_name)
-    print "\n"
+    puts "wc: #{input_str}: open: No such file or directory"
   end
 end
 
-def output_wc_l(file_name)
-  if File.exist?(file_name[0])
-    file_name.each do |a|
+def output_wc_l(input_str)
+  if File.exist?(input_str[0])
+    input_str.each do |a|
       file = File.read(a)
       file_lines = file.count("\n").to_s.rjust(8)
       name = File.basename(a)
       puts "#{file_lines} #{name}"
     end
-    return unless file_name.size > 1
+    return unless input_str.size > 1
 
-    total_lines_point(file_name)
+    total_lines_point(input_str)
     print " total\n"
 
   else
-    total_lines_point_nofile(file_name)
-    print "\n"
+    puts "wc: #{input_str}: open: No such file or directory"
   end
 end
 
-def total_lines_point(file_name)
+def total_lines_point(input_str)
   total_file_lines = []
-  file_name.each do |a|
+  input_str.each do |a|
     file = File.read(a)
     total_file_lines << file.count("\n")
   end
   print total_file_lines.inject(:+).to_s.rjust(8)
 end
 
-def total_words_point(file_name)
+def total_words_point(input_str)
   total_file_words = []
-  file_name.each do |a|
+  input_str.each do |a|
     file = File.read(a)
     words = file.split(/\s+/)
     total_file_words << words.size
@@ -72,33 +68,15 @@ def total_words_point(file_name)
   print total_file_words.inject(:+).to_s.rjust(8)
 end
 
-def total_bytesize_point(file_name)
+def total_bytesize_point(input_str)
   total_file_bytesize = []
-  file_name.each do |a|
+  input_str.each do |a|
     file = File.read(a)
     total_file_bytesize << file.bytesize
   end
   print total_file_bytesize.inject(:+).to_s.rjust(8)
 end
 
-def total_lines_point_nofile(file_name)
-  print file_name.size.to_s.rjust(8)
-end
 
-def total_words_point_nofile(file_name)
-  total_words = []
-  file_name.each_with_index do |_a, i|
-    total_words << file_name[i].split(/\s+/).size
-  end
-  print total_words.inject(:+).to_s.rjust(8)
-end
-
-def total_bytesize_point_nofile(file_name)
-  total_bytesize = []
-  file_name.each_with_index do |_a, i|
-    total_bytesize << file_name[i].bytesize
-  end
-  print total_bytesize.inject(:+).to_s.rjust(8)
-end
 
 main
