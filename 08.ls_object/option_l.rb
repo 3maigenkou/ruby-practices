aada0d4170e1eb6047cb5e0dfb96828aaf478499
3maigenkou <kaipurio848@gmail.com>
2023-04-06 08:36:50 +0900

    パーミッションの追加

--- 08.ls_object/option_l.rb
+++ 08.ls_object/option_l.rb
@@ -1,20 +1,64 @@
 # frozen_string_literal: true
 
+require 'etc'
+
 class OptionL
-  def initialize(files_name)
-    @files_data = files_name.map { |data| File::Stat.new(data) }
+  def output
+    @files_name
   end
 
-  def output
-    p file_type1
+# private
+
+  def initialize(files_name)
+    @files_name = files_name
+    @files_data = files_name.map { |data| File::Stat.new(data) }
   end
 
   def total
     @files_data.map(&:blocks).sum
   end
 
-  def file_type
-    @files_data.map(&:ftype).map(&:to_sym)
+  def files_type
+    files_type = @files_data.map(&:ftype).map(&:to_sym)
+    files_type.map { |file| convert_file_type(file) }
+  end
+
+  def file_mode
+    @files_data.map { |file| format('%o', file.mode)}
+  end
+
+  def file_permission_owner
+    owner_permission_number = file_mode.map { |mode|mode[-3] }
+    owner_permission_number_sym = owner_permission_number.map(&:to_sym)
+    owner_permission_number_sym.map {|number|convert_permission(number)}
+  end
+
+  def file_permission_group
+    group_permission_number = file_mode.map { |mode|mode[-2] }
+    group_permission_number_sym = group_permission_number.map(&:to_sym)
+    group_permission_number_sym.map {|number|convert_permission(number)}
+  end
+
+  def file_permission_other
+    other_permission_number = file_mode.map { |mode|mode[-1] }
+    other_permission_number_sym = other_permission_number.map(&:to_sym)
+    other_permission_number_sym.map {|number|convert_permission(number)}
+  end
+
+  def file_nlink
+    @files_data.map(&:nlink)
+  end
+
+  def owner_name
+  end
+
+  def group_name
+  end
+
+  def file_size
+  end
+
+  def time_stamp
   end
 
   def convert_file_type(file_type)
